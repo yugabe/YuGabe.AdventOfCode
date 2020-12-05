fec4e8b0d95ce3158b2e96a1376b75c7bda2353d
Szabó Gábor <szabo.gabor@aut.bme.hu>
2020-12-05 08:08:31 +0100

    2020-5

---
+++ YuGabe.AdventOfCode.Year2020/Day5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tidy.AdventOfCode;
+
+namespace YuGabe.AdventOfCode.Year2020
+{
+    class Day5 : Day<HashSet<int>>
+    {
+        public override HashSet<int> ParseInput(string rawInput) => 
+            rawInput.Split('\n').Select(i => Convert.ToInt32(i.Replace('F', '0').Replace('B', '1').Replace('R', '1').Replace('L', '0'), 2)).ToHashSet();
+
+        public override object ExecutePart1() => Input.Max();
+
+        public override object ExecutePart2() => Input.First(i => !Input.Contains(i - 1) && Input.Contains(i - 2) && (i >> 3) is not 0b1 and not 0b1111111) - 1;
+    }
+}
