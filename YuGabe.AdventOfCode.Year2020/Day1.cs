0684de9de977aad578e73920c4e33d290ec22c75
Szabó Gábor <szabo.gabor@aut.bme.hu>
2020-12-16 11:22:51 +0100

    2020 Day 1 cleanup

--- YuGabe.AdventOfCode.Year2020/Day1.cs
+++ YuGabe.AdventOfCode.Year2020/Day1.cs
@@ -3,20 +3,18 @@ using Tidy.AdventOfCode;
 
 namespace YuGabe.AdventOfCode.Year2020
 {
-    class Day1 : Day.NewLineSplitParsed<int>
+    public class Day1 : Day.NewLineSplitParsed<int>
     {
         public override object ExecutePart1()
         {
             var set = Input.ToHashSet();
-            var pair = set.Select(num => (num, diff: 2020 - num)).First(i => set.Contains(i.diff));
-            return pair.num * pair.diff;
+            return set.Select(num => (num, diff: 2020 - num)).First(i => set.Contains(i.diff)).FeedTo((num, diff) => num * diff);
         }
 
         public override object ExecutePart2()
         {
             var set = Input.ToHashSet();
-            var pair = set.SelectMany(num => set.Select(other => (num, other, diff: 2020 - num - other))).First(i => set.Contains(i.diff));
-            return pair.num * pair.other * pair.diff;
+            return set.SelectMany(num => set.Select(other => (num, other, diff: 2020 - num - other))).First(i => set.Contains(i.diff)).FeedTo((num, other, diff) => num * other * diff);
         }
     }
 }
