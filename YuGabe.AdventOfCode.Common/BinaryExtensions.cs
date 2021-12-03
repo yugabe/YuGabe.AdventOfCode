3c0d60b083737bf1fb2c754e87d314b4d76a414b
Gábor Szabó <gabe.szabo@outlook.com>
2021-12-03 16:56:04 +0100

    Solution: 2021-3

---
+++ YuGabe.AdventOfCode.Common/BinaryExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace YuGabe.AdventOfCode;
+
+public static class BinaryExtensions
+{
+    public static byte ToByte(this IEnumerable<bool> bits)
+        => bits.Aggregate((byte)0, (acc, c) => (byte)(acc * 2 + (c ? 1 : 0)));
+
+    public static int ToInt(this IEnumerable<bool> bits)
+        => bits.Aggregate(0, (acc, c) => acc * 2 + (c ? 1 : 0));
+
+    public static long ToLong(this IEnumerable<bool> bits)
+        => bits.Aggregate(0L, (acc, c) => acc * 2 + (c ? 1 : 0));
+}
---
+++ YuGabe.AdventOfCode.Year2021/Day3.cs
@@ -0,0 +1,40 @@
+﻿namespace YuGabe.AdventOfCode.Year2021;
+
+public class Day3 : Day<bool[][]>
+{
+    public override object ExecutePart1()
+    {
+        var gamma = Enumerable.Range(0, Input[0].Length).Select(i => Input.Count(r => r[i]) > Input.Length / 2).ToInt();
+        var epsilon = Math.Pow(2, Input[0].Length) - 1 - gamma;
+        var consumption = gamma * epsilon;
+        return consumption;
+    }
+
+    public override object ExecutePart2()
+    {
+        var (o2GeneratorNumbers, co2ScrubberNumbers) = (Input.ToArray(), Input.ToArray());
+
+        for (var i = 0; i < Input[0].Length; i++)
+        {
+            if (o2GeneratorNumbers.Length * co2ScrubberNumbers.Length == 0)
+                throw null!;
+
+            if (o2GeneratorNumbers.Length > 1)
+            {
+                var value = o2GeneratorNumbers.Count(r => r[i]) >= o2GeneratorNumbers.Count(r => !r[i]);
+                o2GeneratorNumbers = o2GeneratorNumbers.Where(r => r[i] == value).ToArray();
+            }
+
+            if (co2ScrubberNumbers.Length > 1)
+            {
+                var value = co2ScrubberNumbers.Count(r => r[i]) < co2ScrubberNumbers.Count(r => !r[i]);
+                co2ScrubberNumbers = co2ScrubberNumbers.Where(r => r[i] == value).ToArray();
+            }
+        }
+
+        return o2GeneratorNumbers.Single().ToInt() * co2ScrubberNumbers.Single().ToInt();
+    }
+
+    public override bool[][] ParseInput(string rawInput)
+        => rawInput.GetLines().Select(l => l.Select(c => c == '1').ToArray()).ToArray();
+}
