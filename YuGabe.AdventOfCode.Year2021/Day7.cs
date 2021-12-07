85fd2a67acded6cc523c923ceb2a95eaa87dc2ea
Gábor Szabó <gabe.szabo@outlook.com>
2021-12-07 11:56:11 +0100

    Solution: 2021-7

---
+++ YuGabe.AdventOfCode.Year2021/Day7.cs
@@ -0,0 +1,11 @@
+﻿namespace YuGabe.AdventOfCode.Year2021;
+
+public class Day7 : Day<int[]>
+{
+    public override int[] ParseInput(string rawInput) => rawInput.ToMany<int>(",");
+    private object Execute(Func<int, int, double> sumSelector)
+        => Input.MinMax() is var (min, max) ? Enumerable.Range(min, max - min + 1).Min(pos => Input.Sum(c => sumSelector(pos, c))) : 0;
+
+    public override object ExecutePart1() => Execute(static (pos, c) => Math.Abs(c - pos));
+    public override object ExecutePart2() => Execute(static (pos, c) => Math.Abs(c - pos) is var d ? ((((double)d - 1) / 2) + 1) * d : 0);
+}
