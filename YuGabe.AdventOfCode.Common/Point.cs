1334a3110f859319f428d9642d1b0b29c144ee76
Gábor Szabó <gabe.szabo@outlook.com>
2021-12-09 13:56:41 +0100

    Solution: 2021-9

---
+++ YuGabe.AdventOfCode.Common/Point.cs
@@ -0,0 +1,5 @@
+﻿namespace YuGabe.AdventOfCode;
+
+public record Point2D<T>(T X, T Y) { }
+public record Point3D<T>(T X, T Y, T Z) { }
+public record Point4D<T>(T X, T Y, T Z, T W) { }
---
+++ YuGabe.AdventOfCode.Year2021/Day9.cs
@@ -0,0 +1,21 @@
+﻿namespace YuGabe.AdventOfCode.Year2021;
+
+using KV = KeyValuePair<Point2D<int>, int>;
+
+public class Day9 : Day<Dictionary<Point2D<int>, int>>
+{
+    public override Dictionary<Point2D<int>, int> ParseInput(string rawInput) => rawInput.SplitAtNewLines().WithIndexes().SelectMany(line => line.Element.WithIndexes()
+            .Select(c => (x: c.Index, y: line.Index, value: int.Parse(c.Element.ToString()))))
+            .ToDictionary(e => new Point2D<int>(e.x, e.y), e => e.value);
+
+    public IEnumerable<KV> GetNeighbors(Point2D<int> p) => new Point2D<int>[] { new(p.X - 1, p.Y), new(p.X + 1, p.Y), new(p.X, p.Y - 1), new(p.X, p.Y + 1) }
+        .Where(Input.ContainsKey).Select(k => new KV(k, Input[k]));
+
+    public IEnumerable<KV> GetAllLowPoints() => Input.Where(p => GetNeighbors(p.Key).All(n => n.Value > p.Value));
+
+    public override object ExecutePart1() => GetAllLowPoints().Sum(p => p.Value + 1);
+
+    public IEnumerable<KV> GetBasinElements(KV point) => GetNeighbors(point.Key).Where(p => p.Value > point.Value && p.Value != 9).SelectMany(GetBasinElements).Prepend(point).Distinct();
+
+    public override object ExecutePart2() => GetAllLowPoints().Select(k => GetBasinElements(k).Count()).OrderByDescending(s => s).Take(3).Aggregate((p, c) => p * c);
+}
