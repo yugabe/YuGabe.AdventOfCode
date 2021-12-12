2f80db9bc45803677fb4f23ae96960c6684ee193
Gábor Szabó <gabe.szabo@outlook.com>
2021-12-12 20:59:04 +0100

    2015-17 solution

--- YuGabe.AdventOfCode/Year2015/Day17.cs
+++ YuGabe.AdventOfCode/Year2015/Day17.cs
@@ -2,48 +2,23 @@
 
 public class Day17 : Day.NewLineSplitParsed<int>
 {
-    public const int Target = 150;
-
-    public static IEnumerable<IEnumerable<int>> Permutate(IEnumerable<int> source, int remainder)
+    public static IEnumerable<IEnumerable<(int number, int id)>> Permutate(IEnumerable<(int number, int id)> source, int remainder)
     {
-        if (!source.Any())
-            yield break;
-        var first = source.First();
-        if (source.Skip(1).Any())
+        foreach (var (number, id) in source)
         {
-            foreach (var permutation in source.SelectMany((element, index) => source.Where((_, i) => i != index).Permutate().Select(others => others.Prepend(element))))
-                yield return permutation;
+            if (remainder == number)
+                yield return new[] { (number, id) };
+            foreach (var permutation in Permutate(source.Where(e => e.id > id && e.number <= remainder - number), remainder - number))
+                yield return permutation.Prepend((number, id));
         }
-        else
-            yield return new[] { first };
     }
 
-    public override object ExecutePart1()
+    public override object ExecutePart1() => Permutate(Input.WithIndexes(), 150).Count();
+
+    public override object ExecutePart2()
     {
-        var matchingIndexes = new HashSet<string>();
-
-        foreach (var i in Enumerable.Range(0, Input.Length))
-        {
-            var sw = System.Diagnostics.Stopwatch.StartNew();
-            var count = Enumerable.Range(0, i).Permutate().Count();
-            Console.WriteLine($"{i}: {sw.ElapsedMilliseconds} ms ({count} items)");
-        }
-
-        foreach (var permutation in Input.Permutate())
-        {
-            var sum = 0;
-            foreach (var (element, index) in permutation.WithIndexes())
-            {
-                sum += element;
-                if (sum == Target)
-                    matchingIndexes.Add(string.Join(",", permutation.Take(index + 1)));
-                if (sum >= Target)
-                    break;
-            }
-        }
-
-        return matchingIndexes.Count;
+        var permutations = Permutate(Input.WithIndexes(), 150).ToList();
+        var leastItems = permutations.Min(p => p.Count());
+        return permutations.Count(p => p.Count() == leastItems);
     }
-
-    public override object ExecutePart2() => null;
 }
