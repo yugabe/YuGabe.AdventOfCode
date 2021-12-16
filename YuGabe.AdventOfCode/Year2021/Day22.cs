903a9b9d6d6fd0f63b2993b4ef703bad617a0c90
Gábor Szabó <gabe.szabo@outlook.com>
2021-12-16 14:26:40 +0100

    2021 remaining solution placeholders

---
+++ YuGabe.AdventOfCode/Year2021/Day22.cs
@@ -0,0 +1,9 @@
+﻿namespace YuGabe.AdventOfCode.Year2021;
+
+public class Day22 : Day
+{
+    public override string ParseInput(string rawInput) => rawInput;
+
+    public override object ExecutePart1() => throw null!;
+    public override object ExecutePart2() => throw null!;
+}
