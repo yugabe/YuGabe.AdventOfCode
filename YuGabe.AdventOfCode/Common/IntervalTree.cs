0094b37da5db9d11adaed7af9007f17e703fd2f7
Gábor Szabó <gabe.szabo@outlook.com>
2021-12-23 10:51:59 +0100

    2021-22 solution

---
+++ YuGabe.AdventOfCode/Common/IntervalTree.cs
@@ -0,0 +1,48 @@
+﻿namespace YuGabe.AdventOfCode;
+
+public class IntervalTree<T, TKey> where TKey : notnull
+{
+    public IntervalTree(IReadOnlyCollection<T> elements, Func<T, TKey> intervalStartSelector, Func<T, TKey> intervalEndSelector, Func<TKey, TKey, TKey> centerFromMinMaxSelector, IComparer<TKey>? comparer = null)
+    {
+        Comparer = (comparer ??= Comparer<TKey>.Default);
+        if (elements.Any(e => Comparer.Compare(intervalStartSelector(e), Center) > 0 && Comparer.Compare(intervalEndSelector(e), Center) < 0))
+            throw new InvalidOperationException();
+        Min = elements.Min(intervalStartSelector) ?? throw new InvalidOperationException();
+        Max = elements.Max(intervalEndSelector) ?? throw new InvalidOperationException();
+        Center = centerFromMinMaxSelector(Min, Max); //(Min + Max) / 2;
+        var leftElements = elements.Where(e => Comparer.Compare(intervalEndSelector(e), Center) < 0).ToArray();
+        if (leftElements.Length > 0)
+            LeftTree = new IntervalTree<T, TKey>(leftElements, intervalStartSelector, intervalEndSelector, centerFromMinMaxSelector, comparer);
+        var rightElements = elements.Where(e => Comparer.Compare(intervalStartSelector(e), Center) > 0).ToArray();
+        if (rightElements.Length > 0)
+            RightTree = new IntervalTree<T, TKey>(rightElements, intervalStartSelector, intervalEndSelector, centerFromMinMaxSelector, comparer);
+        var centerItems = elements.Where(e => Comparer.Compare(intervalEndSelector(e), Center) > 0 && Comparer.Compare(intervalStartSelector(e), Center) < 0).ToArray();
+        CenterItemsByBeginning = centerItems.OrderBy(intervalStartSelector).ToArray();
+        CenterItemsByEnd = centerItems.OrderBy(intervalEndSelector).ToArray();
+        IntervalStartSelector = intervalStartSelector;
+        IntervalEndSelector = intervalEndSelector;
+    }
+
+    public IComparer<TKey> Comparer { get; }
+    public TKey Min { get; }
+    public TKey Center { get; }
+    public TKey Max { get; }
+    public IntervalTree<T, TKey>? LeftTree { get; }
+    public IEnumerable<T> CenterItemsByBeginning { get; }
+    public IEnumerable<T> CenterItemsByEnd { get; }
+    public IntervalTree<T, TKey>? RightTree { get; }
+    private Func<T, TKey> IntervalStartSelector { get; }
+    private Func<T, TKey> IntervalEndSelector { get; }
+
+    public IEnumerable<T> this[TKey key]
+    {
+        get
+        {
+            if (Comparer.Compare(key, Center) < 0)
+                return (LeftTree?[key] ?? Empty<T>()).Concat(CenterItemsByEnd.Where(e => Comparer.Compare(IntervalStartSelector(e), key) < 0));
+            else if (Comparer.Compare(key, Center) > 0)
+                return CenterItemsByEnd.Where(e => Comparer.Compare(IntervalEndSelector(e), key) > 0).Concat(RightTree?[key] ?? Empty<T>());
+            return CenterItemsByBeginning;
+        }
+    }
+}
--- YuGabe.AdventOfCode/Year2021/Day22.cs
+++ YuGabe.AdventOfCode/Year2021/Day22.cs
@@ -1,9 +1,45 @@
 ﻿namespace YuGabe.AdventOfCode.Year2021;
+using Point = Point3D<int>;
 
-public class Day22 : Day
+public class Day22 : Day<(Day22.ToggleState State, Point A, Point B)[]>
 {
-    public override string ParseInput(string rawInput) => rawInput;
+    public enum ToggleState { Off = 0, On = 1 }
+    public override (ToggleState State, Point A, Point B)[] ParseInput(string rawInput) => rawInput.SplitAtNewLines().Select(line => new string(line.Where(c => char.IsDigit(c) || c is '.' or ',' or '-').ToArray()).FeedTo(e => e).Split(new[] { "..", "," }, SSO.None).Select(e => int.Parse(e)).ToArray().FeedTo(tokens => (line.StartsWith("on") ? ToggleState.On : line.StartsWith("off") ? ToggleState.Off : throw new InvalidOperationException(), new Point(tokens[0], tokens[2], tokens[4]), new Point(tokens[1], tokens[3], tokens[5])))).ToArray();
 
-    public override object ExecutePart1() => throw null!;
-    public override object ExecutePart2() => throw null!;
+    public static long Execute((ToggleState State, Point A, Point B)[] toggles)
+    {
+        var (xes, ys, zs) = (toggles.SelectMany(t => new[] { t.A.X, t.B.X, t.A.X + 1, t.B.X + 1 }).Distinct().OrderBy(v => v).ToArray(), toggles.SelectMany(t => new[] { t.A.Y, t.B.Y, t.A.Y + 1, t.B.Y + 1 }).Distinct().OrderBy(v => v).ToArray(), toggles.SelectMany(t => new[] { t.A.Z, t.B.Z, t.A.Z + 1, t.B.Z + 1 }).Distinct().OrderBy(v => v).ToArray());
+        (toggles, var axOrderedToggles, var sum) = (toggles = toggles.Reverse().ToArray(), toggles.WithIndexes().OrderBy(t => t.Element.A.X).ToList(), 0L);
+
+        Parallel.For(0, xes.Length, xi =>
+        {
+            for (var yi = 0; yi < ys.Length; yi++)
+                for (var zi = 0; zi < zs.Length; zi++)
+                {
+                    var (x, y, z) = (xes[xi], ys[yi], zs[zi]);
+                    int? lastGoodIndex = null;
+
+                    foreach (var ((State, A, B), index) in axOrderedToggles)
+                    {
+                        if (A.X > x)
+                            break;
+                        if (/* A.X > x && */B.X >= x && A.Y <= y && B.Y >= y && A.Z <= z && B.Z >= z)
+                            lastGoodIndex = lastGoodIndex == null ? index : Math.Min(lastGoodIndex.Value, index);
+                    }
+                    if (lastGoodIndex != null && toggles[lastGoodIndex.Value].State == ToggleState.On)
+                    {
+                        var xd = (long)Math.Abs((xi == xes.Length - 1 ? x + 1 : xes[xi + 1]) - x);
+                        var yd = (long)Math.Abs((yi == ys.Length - 1 ? y + 1 : ys[yi + 1]) - y);
+                        var zd = (long)Math.Abs((zi == zs.Length - 1 ? z + 1 : zs[zi + 1]) - z);
+                        Interlocked.Add(ref sum, xd * yd * zd);
+                    }
+                }
+        });
+
+        return sum;
+    }
+
+    public override object ExecutePart1() => Execute(Input.Where(ps => new[] { ps.A.X, ps.B.X, ps.A.Y, ps.B.Y, ps.A.Z, ps.B.Z }.All(v => v is >= -50 and <= 50)).ToArray());
+
+    public override object ExecutePart2() => Execute(Input);
 }
