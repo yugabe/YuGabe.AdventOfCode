0b24ccdba74bbd2688630d1c31b4edf866d33f69
Gábor Szabó <gabe.szabo@outlook.com>
2022-12-06 08:34:50 +0100

    2022-6 solution

--- YuGabe.AdventOfCode/Year2022/Day6.cs
+++ YuGabe.AdventOfCode/Year2022/Day6.cs
@@ -1,13 +1,9 @@
 namespace YuGabe.AdventOfCode.Year2022;
 public class Day6 : Day
 {
-    public override object ExecutePart1()
-    {
-        throw new NotImplementedException();
-    }
+    public override object ExecutePart1() => Execute(4);
 
-    public override object ExecutePart2()
-    {
-        throw new NotImplementedException();
-    }
+    public override object ExecutePart2() => Execute(14);
+
+    public int Execute(int markerLength) => Input.WithIndexes().Skip(markerLength - 1).First(e => Input.Skip(e.Index - (markerLength - 1)).Take(markerLength).ToHashSet().Count == markerLength).Index + 1;
 }
