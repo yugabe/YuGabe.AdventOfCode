9eb8807060c4595c31abc3603e93f0410d462df1
Gábor Szabó <gabe.szabo@outlook.com>
2022-12-08 10:18:17 +0100

    DynamicEnumerable, Map2D helpers

---
+++ YuGabe.AdventOfCode/Common/DynamicEnumerable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace YuGabe.AdventOfCode.Common;
+public class DynamicEnumerable<T> : IEnumerable<T>
+{
+    public DynamicEnumerable(T startValue, Func<T, T?> next, Func<T?, bool>? breakCondition = null)
+    {
+        StartValue = startValue;
+        Next = next;
+        BreakCondition = breakCondition;
+    }
+
+    private T StartValue { get; }
+    private Func<T, T?> Next { get; }
+    private Func<T?, bool>? BreakCondition { get; }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var current = StartValue;
+        while (true)
+        {
+            yield return current;
+            current = Next(current);
+            if (current == null || BreakCondition?.Invoke(current) == true)
+                yield break;
+        }
+    }
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
---
+++ YuGabe.AdventOfCode/Common/Map2D.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace YuGabe.AdventOfCode.Common;
+public class Map2D<T> : IReadOnlyDictionary<(int X, int Y), Map2D<T>.Node>
+{
+    public sealed class Node
+    {
+        public Node(Map2D<T> map, (int X, int Y) key, T value) => (Map, Key, Value) = (map, key, value);
+
+        private Map2D<T> Map { get; }
+        public (int X, int Y) Key { get; }
+        public int X => Key.X;
+        public int Y => Key.Y;
+        public T Value { get; set; }
+
+        public Node? Top => Map.GetValueOrDefault((X, Y - 1));
+        public Node? Right => Map.GetValueOrDefault((X + 1, Y));
+        public Node? Down => Map.GetValueOrDefault((X, Y + 1));
+        public Node? Left => Map.GetValueOrDefault((X - 1, Y));
+        public IEnumerable<Node> AllTop => new DynamicEnumerable<Node>(this, c => c.Top).Skip(1);
+        public IEnumerable<Node> AllRight => new DynamicEnumerable<Node>(this, c => c.Right).Skip(1);
+        public IEnumerable<Node> AllDown => new DynamicEnumerable<Node>(this, c => c.Down).Skip(1);
+        public IEnumerable<Node> AllLeft => new DynamicEnumerable<Node>(this, c => c.Left).Skip(1);
+        public IEnumerable<IEnumerable<Node>> AllCardinalNeighbors
+        {
+            get
+            {
+                yield return AllTop;
+                yield return AllRight;
+                yield return AllDown;
+                yield return AllLeft;
+            }
+        }
+        public IEnumerable<Node?> Neighbors
+        {
+            get
+            {
+                yield return Top;
+                yield return Right;
+                yield return Down;
+                yield return Left;
+            }
+        }
+    }
+
+    public Map2D(Dictionary<(int X, int Y), T> map)
+    {
+        Map = map.ToDictionary(kv => kv.Key, kv => new Node(this, kv.Key, kv.Value)).AsReadOnly();
+        ((MinX, MinY), (MaxX, MaxY)) = map.Keys.MinMax();
+    }
+
+    private IReadOnlyDictionary<(int X, int Y), Node> Map { get; }
+
+    public IEnumerable<(int X, int Y)> Keys => Map.Keys;
+    public IEnumerable<Map2D<T>.Node> Values => Map.Values;
+    public int Count => Map.Count;
+
+    public int MinX { get; }
+    public int MinY { get; }
+    public int MaxX { get; }
+    public int MaxY { get; }
+
+    public IEnumerable<Map2D<T>.Node> GetRow(int y) => Range(MinX, MaxX - MinX).Select(x => this[x, y]);
+    public IEnumerable<Map2D<T>.Node> GetColumn(int x) => Range(MinY, MaxY - MinY).Select(y => this[x, y]);
+
+    public Node this[(int X, int Y) key] => Map[key];
+    public Node this[int X, int Y] => Map[(X, Y)];
+
+    public bool ContainsKey((int X, int Y) key) => Map.ContainsKey(key);
+    public bool TryGetValue((int X, int Y) key, [MaybeNullWhen(false)] out Map2D<T>.Node value) => Map.TryGetValue(key, out value);
+    public IEnumerator<KeyValuePair<(int X, int Y), Map2D<T>.Node>> GetEnumerator() => Map.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => Map.GetEnumerator();
+}
