ae3f25cf4905fd989ebf02a49c528e23bdeec8c2
Gábor Szabó <gabe.szabo@outlook.com>
2022-12-08 10:18:24 +0100

    2022-8 solution

--- YuGabe.AdventOfCode/Year2022/Day8.cs
+++ YuGabe.AdventOfCode/Year2022/Day8.cs
@@ -1,13 +1,11 @@
-namespace YuGabe.AdventOfCode.Year2022;
-public class Day8 : Day
-{
-    public override object ExecutePart1()
-    {
-        throw new NotImplementedException();
-    }
+using YuGabe.AdventOfCode.Common;
 
-    public override object ExecutePart2()
-    {
-        throw new NotImplementedException();
-    }
+namespace YuGabe.AdventOfCode.Year2022;
+public class Day8 : Day<Map2D<int>>
+{
+    public override Map2D<int> ParseInput(string rawInput) => new(rawInput.SplitAtNewLines().SelectMany((row, y) => row.Select((c, x) => (value: c - 48, x, y))).ToDictionary(e => (e.x, e.y), e => e.value));
+
+    public override object ExecutePart1() => Input.Values.Count(tree => tree.AllCardinalNeighbors.Any(dir => dir.All(o => o.Value < tree.Value)));
+
+    public override object ExecutePart2() => base.Input.Values.Max(tree => tree.AllCardinalNeighbors.Aggregate(1, (acc, dir) => dir.WithNeighbors().TakeWhile(o => (o.previous?.Value ?? 0) < tree.Value).Count()));
 }
