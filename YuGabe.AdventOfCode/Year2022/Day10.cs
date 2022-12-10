00431e25dddc4aadb6acb24584ed2daaee8f21be
Gábor Szabó <gabe.szabo@outlook.com>
2022-12-10 12:25:45 +0100

    2022-10 solution

--- YuGabe.AdventOfCode/Year2022/Day10.cs
+++ YuGabe.AdventOfCode/Year2022/Day10.cs
@@ -1,13 +1,40 @@
 namespace YuGabe.AdventOfCode.Year2022;
-public class Day10 : Day
+public class Day10 : Day<(string Instruction, int? Value)[]>
 {
+    public override (string Instruction, int? Value)[] ParseInput(string rawInput) => rawInput.SplitAtNewLines().Select(l => l.Split(" ")).Select(l => (l[0], int.TryParse(l.ElementAtOrDefault(1), out var value) ? (int?)value : null)).ToArray();
+
+    private IReadOnlyDictionary<string, int> InstructionCosts { get; } = new Dictionary<string, int> { ["addx"] = 2, ["noop"] = 1 };
+
     public override object ExecutePart1()
     {
-        throw new NotImplementedException();
+        var sum = 0;
+        for (var (cycle, pc, x) = (0, 0, 1); pc < Input.Length; pc++)
+        {
+            var (instruction, value) = Input[pc];
+            var cost = InstructionCosts[instruction];
+            for (var tick = 0; tick < cost; tick++)
+                if (++cycle % 40 == 20)
+                    sum += cycle * x;
+            if (instruction == "addx")
+                x += value!.Value;
+        }
+        return sum;
     }
 
     public override object ExecutePart2()
     {
-        throw new NotImplementedException();
+        for (var (cycle, pc, x) = (1, 0, 1); pc < Input.Length; pc++)
+        {
+            var end = cycle + InstructionCosts[Input[pc].Instruction];
+            for (; cycle < end; cycle++)
+            {
+                if (cycle % 40 == 1)
+                    Console.WriteLine();
+                Console.Write((cycle % 40) - x is >= 0 and <= 2 ? '#' : '.');
+            }
+            if (Input[pc] is ("addx", int value))
+                x += value;
+        }
+        throw null!;
     }
 }
