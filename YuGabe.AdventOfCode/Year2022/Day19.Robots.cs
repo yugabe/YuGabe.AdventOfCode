2d2625c709cba4c5342a1f00d3a7c8f6b76965cb
Gábor Szabó <gabe.szabo@outlook.com>
2022-12-23 16:32:29 +0100

    2022-19 solution

---
+++ YuGabe.AdventOfCode/Year2022/Day19.Robots.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Concurrent;
+using static YuGabe.AdventOfCode.Year2022.Day19.ResourceType;
+
+namespace YuGabe.AdventOfCode.Year2022;
+public partial class Day19
+{
+    public enum ResourceType { Geode, Obsidian, Clay, Ore }
+    public record StateOG(int MinutesRemaining, Dictionary<ResourceType, int> Robots, Dictionary<ResourceType, int> Resources);
+    public record BlueprintOG(int Number, Dictionary<ResourceType, Dictionary<ResourceType, int>> RobotCosts);
+    public static BlueprintOG[] ParseInputOG(string rawInput) => rawInput.Split("\n").Select(l => l.Split(':', ' ').Where(t => int.TryParse(t, out _)).Select(int.Parse).ToArray()).Select(t => new BlueprintOG(t[0], new()
+    {
+        [Ore] = new() { [Ore] = t[1], [Clay] = 0, [Obsidian] = 0, [Geode] = 0 },
+        [Clay] = new() { [Ore] = t[2], [Clay] = 0, [Obsidian] = 0, [Geode] = 0 },
+        [Obsidian] = new() { [Ore] = t[3], [Clay] = t[4], [Obsidian] = 0, [Geode] = 0 },
+        [Geode] = new() { [Ore] = t[5], [Clay] = 0, [Obsidian] = t[6], [Geode] = 0 },
+    })).ToArray();
+
+    public static IReadOnlyDictionary<BlueprintOG, int> ExecuteOG(BlueprintOG[] blueprints, int minutes)
+    {
+        var startingState = new StateOG(minutes, new() { [Ore] = 1, [Clay] = 0, [Obsidian] = 0, [Geode] = 0 }, new() { [Ore] = 0, [Clay] = 0, [Obsidian] = 0, [Geode] = 0 });
+        var queues = blueprints.Select(b => new PriorityQueue<(StateOG State, BlueprintOG Blueprint), int>(new[] { ((startingState, b), int.MinValue) })).ToList();
+        var maxGeodesByBlueprint = new ConcurrentDictionary<BlueprintOG, int>();
+        var results = Parallel.ForEach(blueprints, new() { MaxDegreeOfParallelism = Environment.ProcessorCount }, blueprint =>
+        {
+            var queue = new PriorityQueue<StateOG, int>(new[] { (startingState, int.MinValue) });
+            var max = 0;
+            while (queue.TryDequeue(out var state, out var priority))
+            {
+                var geodeRobots = state.Robots[Geode];
+                if (geodeRobots > 0)
+                {
+                    var newMax = (geodeRobots * state.MinutesRemaining) + state.Resources[Geode];
+                    if (newMax > max)
+                    {
+                        max = newMax;
+                        Console.WriteLine($"Blueprint #{blueprint.Number}: {newMax}");
+                    }
+                }
+
+                if (GetGeodeUpperBoundsOG(state) is var bounds && bounds <= max)
+                    continue;
+
+                foreach (var (robotType, costs) in blueprint.RobotCosts)
+                {
+                    var minutesForCosts = costs.Select(rc =>
+                    {
+                        var neededResources = rc.Value - state.Resources[rc.Key];
+                        if (neededResources <= 0)
+                            return (int?)0; // already available
+                        if (state.Robots[rc.Key] is var robotsCount && robotsCount > 0)
+                            return (int)Math.Ceiling((double)neededResources / robotsCount); // time for current robots to make deficit
+                        return null; // unobtainable
+                    }).ToList();
+                    if (minutesForCosts.All(m => m != null))
+                    {
+                        var minutesToWait = 1 + minutesForCosts.Max() ?? throw new Exception("All costs should be obtainable at this point.");
+                        if (minutesToWait < state.MinutesRemaining)
+                        {
+                            var nextState = new StateOG(
+                                                            state.MinutesRemaining - minutesToWait,
+                                                            new(state.Robots) { [robotType] = state.Robots[robotType] + 1 },
+                                                            new(state.Resources.ToDictionary(
+                                                                r => r.Key,
+                                                                r => r.Value + (state.Robots[r.Key] * minutesToWait) - costs[r.Key])));
+                            if (GetGeodeUpperBoundsOG(nextState) is var nextBounds && nextBounds > max)
+                                queue.Enqueue(nextState, GetPriorityOG(nextState));
+                        }
+                    }
+                }
+            }
+            maxGeodesByBlueprint[blueprint] = max;
+            Console.WriteLine($" > Blueprint #{blueprint.Number} ({maxGeodesByBlueprint.Count} complete): {max}");
+        });
+        return maxGeodesByBlueprint;
+    }
+
+    public static int GetPriorityOG(StateOG state) =>
+        -((10 * ((state.Robots[Geode] * state.MinutesRemaining) + state.Resources[Geode])) +
+        (5 * ((state.Robots[Obsidian] * state.MinutesRemaining) + state.Resources[Obsidian])) +
+        (3 * ((state.Robots[Clay] * state.MinutesRemaining) + state.Resources[Clay])) +
+        (1 * ((state.Robots[Ore] * state.MinutesRemaining) + state.Resources[Ore])));
+
+    public static int GetGeodeUpperBoundsOG(StateOG state)
+    {
+        var robots = state.Robots[Geode];
+        return state.Resources[Geode] + (robots * state.MinutesRemaining) + Range(robots, state.MinutesRemaining).Sum(r => r * (state.MinutesRemaining + r - robots - 1));
+    }
+
+    //public object ExecutePart1OG() => Execute(Input, 24).Sum(b => b.Key.Number * b.Value);
+
+    //public object ExecutePart2OG() => Execute(Input[..3], 32).Aggregate(1, (acc, kv) => acc * kv.Value);
+}
--- YuGabe.AdventOfCode/Year2022/Day19.cs
+++ YuGabe.AdventOfCode/Year2022/Day19.cs
@@ -1,13 +1,109 @@
+﻿using System.Collections.Concurrent;
+
 namespace YuGabe.AdventOfCode.Year2022;
-public class Day19 : Day
+public partial class Day19 : Day<Day19.Blueprint[]>
 {
-    public override object ExecutePart1()
+    public record Blueprint(short Number, short[][] RobotCosts);
+
+    public override Blueprint[] ParseInput(string rawInput) => rawInput.Split("\n").Select(l => l.Split(':', ' ').Where(t => short.TryParse(t, out _)).Select(short.Parse).ToArray()).Select(t => new Blueprint(t[0], new short[][]
     {
-        throw new NotImplementedException();
+        /*[Geode] = */new short[] { /*[Geode] =*/ 0, /*[Obsidian] =*/ t[6], /*[Clay] = */ 0, /*[Ore] =*/ t[5] },
+        /*[Obsidian] = */new short[] { /*[Geode] =*/ 0, /*[Obsidian] =*/ 0, /*[Clay] =*/ t[4], /*[Ore] =*/ t[3] },
+        /*[Clay] = */new short[] { /*[Geode] =*/ 0, /*[Obsidian] =*/ 0, /*[Clay] =*/ 0, /*[Ore] =*/ t[2] },
+        /*[Ore] = */new short[] { /*[Geode] =*/ 0, /*[Obsidian] =*/ 0, /*[Clay] =*/ 0, /*[Ore] =*/ t[1] },
+    })).ToArray();
+
+    public static IReadOnlyDictionary<Blueprint, short> Execute2(Blueprint[] blueprints, short minutes)
+    {
+        var maxGeodesByBlueprint = new ConcurrentDictionary<Blueprint, short>();
+        Parallel.ForEach(blueprints, blueprint =>
+        {
+            // 0: MinutesRemaining
+            // 1: Robots[Geode]
+            // 2: Robots[Obsidian]
+            // 3: Robots[Clay]
+            // 4: Robots[Ore]
+            // 5: Resources[Geode]
+            // 6: Resources[Obsidian]
+            // 7: Resources[Clay]
+            // 8: Resources[Ore]
+
+            // could try partitioning into queues by modulo of priority?
+            var queue = new PriorityQueue<short[], int>(new[] { (new short[] { minutes, 0, 0, 0, 1, 0, 0, 0, 0 }, int.MinValue) });
+            short max = 0;
+            Span<short> nextState = stackalloc short[9];
+            while (queue.TryDequeue(out var state, out var priority))
+            {
+                var minutesRemaining = state[0];
+                var geodeRobots = state[1];
+                var geodes = state[5];
+                if (geodeRobots > 0)
+                {
+                    var newMax = (short)((geodeRobots * minutesRemaining) + geodes);
+                    if (newMax > max)
+                    {
+                        max = newMax;
+                        Console.WriteLine($"Blueprint #{blueprint.Number}: {newMax}");
+                    }
+                }
+
+                for (var robotType = 0; robotType < 4; robotType++) // robotType is the robot we're trying to build next
+                {
+                    var costs = blueprint.RobotCosts[robotType];
+                    short maxWait = 0;
+                    for (var costType = 0; costType < 4; costType++)
+                    {
+                        var neededResources = costs[costType] - state[5 + costType];
+                        if (neededResources > 0)
+                        {
+                            var robotsCount = state[1 + costType];
+                            if (robotsCount > 0)
+                            {
+                                var waitForCurrentResource = (short)Math.Ceiling((double)neededResources / robotsCount);
+                                if (waitForCurrentResource > maxWait)
+                                {
+                                    if (waitForCurrentResource >= minutesRemaining)
+                                        goto nextRobot; // not enough time to make, try next robotType
+                                    else
+                                        maxWait = waitForCurrentResource;
+                                }
+                            }
+                            else
+                                goto nextRobot; // not enough materials, and no robots to make them
+                        }
+                    }
+
+                    maxWait++;
+                    nextState[0] = (short)(minutesRemaining - maxWait);
+                    nextState[1] = (short)(state[1] + (robotType == 0 ? 1 : 0));
+                    nextState[2] = (short)(state[2] + (robotType == 1 ? 1 : 0));
+                    nextState[3] = (short)(state[3] + (robotType == 2 ? 1 : 0));
+                    nextState[4] = (short)(state[4] + (robotType == 3 ? 1 : 0));
+                    nextState[5] = (short)(state[5] + (state[1] * maxWait) - costs[0]);
+                    nextState[6] = (short)(state[6] + (state[2] * maxWait) - costs[1]);
+                    nextState[7] = (short)(state[7] + (state[3] * maxWait) - costs[2]);
+                    nextState[8] = (short)(state[8] + (state[4] * maxWait) - costs[3]);
+                    var m = nextState[0];
+                    var robots = nextState[1];
+                    var bounds = nextState[5] + (robots * m); // definite number of geodes
+                    for (var r = robots; r < robots + m; r++)
+                    {
+                        bounds += r * (m + r - robots - 1); // if every minute after, we'd build a geode harvester (regardless of whether we can afford to)...
+                        if (bounds > max)
+                        {
+                            queue.Enqueue(nextState.ToArray(), -((nextState[1] * nextState[0]) + nextState[5]));
+                            break;
+                        }
+                    }
+                nextRobot:;
+                }
+            }
+            maxGeodesByBlueprint[blueprint] = max;
+            Console.WriteLine($" > Blueprint #{blueprint.Number} ({maxGeodesByBlueprint.Count} complete): {max}");
+        });
+        return maxGeodesByBlueprint;
     }
 
-    public override object ExecutePart2()
-    {
-        throw new NotImplementedException();
-    }
+    public override object ExecutePart1() => Execute2(Input, 24).Sum(b => b.Key.Number * b.Value);
+    public override object ExecutePart2() => Execute2(Input[..3], 32).Aggregate(1, (acc, kv) => acc * kv.Value);
 }
