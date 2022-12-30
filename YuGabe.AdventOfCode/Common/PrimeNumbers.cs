f9961eb4b16470336decb223d6439e9651838c85
Gábor Szabó <gabe.szabo@outlook.com>
2022-12-30 14:10:48 +0100

    2015-20 solution

---
+++ YuGabe.AdventOfCode/Common/PrimeNumbers.cs
@@ -0,0 +1,70 @@
+﻿namespace YuGabe.AdventOfCode.Common;
+public class PrimeNumbers
+{
+    public PrimeNumbers(long initialPrimeUpperBounds = 23)
+    {
+        GeneratePrimes(initialPrimeUpperBounds);
+    }
+
+    private long _highestPrimeYet = 23;
+    private readonly List<long> _primeList = new() { 2, 3, 5, 7, 11, 13, 17, 19, 23 };
+    private readonly HashSet<long> _primeSet = new() { 2, 3, 5, 7, 11, 13, 17, 19, 23 };
+    public IReadOnlySet<long> PrimeSet => _primeSet;
+    public IReadOnlyList<long> PrimeList => _primeList.AsReadOnly();
+
+    public bool IsPrime(long number)
+    {
+        if (_highestPrimeYet <= number + 1)
+        {
+            return _primeSet.Contains(number);
+        }
+        else
+        {
+            GeneratePrimes(number);
+            return _primeSet.Contains(number);
+        }
+    }
+
+    public IEnumerable<long> GetPrimeFactors(long number)
+    {
+        if (number < 0)
+            throw new ArgumentOutOfRangeException(nameof(number));
+        else if (number == 0)
+            yield break;
+        else if (number == 1)
+            yield return 1;
+        else if (_primeSet.Contains(number))
+            yield return number;
+        else
+        {
+            var sqrt = (long)Math.Sqrt(number);
+            GeneratePrimes(sqrt);
+            foreach (var prime in _primeList)
+            {
+                if (prime > sqrt)
+                    yield break;
+                if ((number % prime) == 0)
+                {
+                    yield return prime;
+                    foreach (var lower in GetPrimeFactors(number / prime))
+                        yield return lower;
+                    yield break;
+                }
+            }
+            yield return number;
+        }
+    }
+
+    public void GeneratePrimes(long upperBounds)
+    {
+        for (var candidate = _highestPrimeYet + 2; candidate <= upperBounds; candidate += 2)
+        {
+            if (candidate % 3 != 0 && candidate % 5 != 0 && candidate % 7 != 0 && candidate % 11 != 0 && candidate % 13 != 0 && candidate % 17 != 0 && candidate % 19 != 0 && candidate % 23 != 0 && _primeList.All(p => (candidate % p) != 0))
+            {
+                _highestPrimeYet = candidate;
+                _primeList.Add(candidate);
+                _primeSet.Add(candidate);
+            }
+        }
+    }
+}
--- YuGabe.AdventOfCode/Year2015/Day20.cs
+++ YuGabe.AdventOfCode/Year2015/Day20.cs
@@ -1,13 +1,26 @@
 namespace YuGabe.AdventOfCode.Year2015;
-public class Day20 : Day
+public class Day20 : Day<int>
 {
-    public override object ExecutePart1()
+    public override int ParseInput(string rawInput) => int.Parse(rawInput);
+
+    public int Execute(Func<int, int, int> deliverPresents)
     {
-        throw new NotImplementedException();
+        var house = 0;
+        while (true)
+        {
+            var presents = 0;
+            for (var elf = 1; elf <= Math.Sqrt(house); elf++)
+                if (house % elf == 0)
+                    presents += deliverPresents(house, elf);
+            if (presents > Input)
+                return house;
+            house += 20;
+        }
     }
 
-    public override object ExecutePart2()
-    {
-        throw new NotImplementedException();
-    }
+    public override object ExecutePart1()
+        => Execute((house, elf) => (elf + (house / elf) is var otherElf && elf != otherElf ? otherElf : 0) * 10);
+
+    public override object ExecutePart2() 
+        => Execute((house, elf) => house / elf is var otherElf ? ((otherElf <= 50 ? elf : 0) + (house / otherElf <= 50 && elf != otherElf ? otherElf : 0)) * 11 : 0);
 }
